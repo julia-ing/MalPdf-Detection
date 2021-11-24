d81ee001c437d3d11cb7aaa019cb4381d568c7a9
julia-ing <ywchoi0625@gmail.com>
2021-11-24 21:29:48 +0900

    VM: run commands in python file

--- app.py
+++ app.py
@@ -6,35 +6,37 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-# 파일 리스트
 @app.route('/list')
 def list_page():
-    file_list = os.listdir("./uploads")
-    html = """<center><a href="/">홈페이지</a><br><br>"""
+    file_list = os.listdir("~/dataset/benign")
+    html = """<center><a href="/">home</a><br><br>"""
     html += "file_list: {}".format(file_list) + "</center>"
     return html
 
 
-# 업로드 HTML 렌더링
 @app.route('/upload')
 def upload_page():
     return render_template('upload.html')
 
 
-# 파일 업로드 처리
 @app.route('/fileUpload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        # 저장할 경로 + 파일명
-        f.save('home/yewon/dataset/benign/' + secure_filename(f.filename))
-        # f.save('./uploads/' + secure_filename(f.filename))
+        #f.save('../../../dataset/benign/' + secure_filename(f.filename))
+        #os.system("ls -d ../../../dataset/benign/* > ~/hidost/build/bpdfs.txt") 
+        #os.system("../src/cacher -i ../bpdfs.txt --compact --values -c /home/yewon/hidost/build/cache-ben/ -t10 -m256")
+        #os.system("find /home/yewon/hidost/build/cache-ben -name '*.pdf' -not -empty > ../cached-bpdfs.txt")
+        #os.system("find /home/yewon/hidost/build/cache-mal -name '*.pdf' -not -empty > ../cached-mpdfs.txt")
+        #os.system("cat ../cached-bpdfs.txt ../cached-mpdfs.txt > ../cached-pdfs.txt")
+        #os.system("../src/pathcount -i ../cached-pdfs.txt -o ../pathcounts.bin")
+        #os.system("../src/feat-select -i ../pathcounts.bin -o ../features.nppf -m1000")
+        #os.system("../src/feat-extract -b ../cached-bpdfs.txt -m ../cached-mpdfs.txt -f ../features.nppf --values -o ../data.libsvm")
         return render_template('check.html')
 
 
